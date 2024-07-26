feec766e38677d79630ae5b59cce484292c86d45
Ayumu3746221 <a.kkt0307@gmail.com>
2024-07-26 19:45:01 +0900

    create Message

--- todo/src/main/java/com/example/todo/controller/TodoController.java
+++ todo/src/main/java/com/example/todo/controller/TodoController.java
@@ -4,18 +4,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.todo.dio.TodoDio;
 import com.example.todo.entity.Todo;
+import com.example.todo.form.RequestedMessage;
 import com.example.todo.form.TodoData;
 import com.example.todo.repository.TodoRepository;
 import com.example.todo.todoservice.TodoService;
 
 import lombok.AllArgsConstructor;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -49,7 +50,7 @@ public class TodoController {
 	}
 	
 	@PutMapping("/todo/{id}/update")
-	public ResponseEntity<String> updateTodo(@PathVariable Integer id,@RequestBody @Validated TodoData requestUpateTodo,BindingResult result) {
+	public List<RequestedMessage> updateTodo(@PathVariable Integer id,@RequestBody @Validated TodoData requestUpateTodo,BindingResult result) {
 		
 		boolean isVaild = todoService.isVaild(requestUpateTodo, result);
 		
@@ -57,27 +58,26 @@ public class TodoController {
 			//Not Error
 			Todo todo = requestUpateTodo.toEntity();
 			todoService.updateTodo(id, todo);
-			
-			return ResponseEntity.ok("OK");
+			return new RequestedMessage(true,null).toMessage();
 		}else {
-			String message = result.getAllErrors().stream().map(error -> error.getDefaultMessage()).collect(Collectors.joining("\n"));
-			return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+			List<String> message = result.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList());
+			return new RequestedMessage(false,message).toMessage();
 		}
 	}
 	
 	@PostMapping("/create/todo")
-	public ResponseEntity<String> creatTodo(@RequestBody @Validated TodoData todoData ,BindingResult result) {
+	public List<RequestedMessage> creatTodo(@RequestBody @Validated TodoData todoData ,BindingResult result) {
 		boolean isVaild = todoService.isVaild(todoData, result);
 		
 		if (!result.hasErrors() && isVaild) {
 			//エラーなし
 			Todo todo = todoData.toEntity();
 			todoRepository.saveAndFlush(todo);
-			return ResponseEntity.ok("OK");
+			return new RequestedMessage(true,null).toMessage();
 		}else {
 			//エラーあり
-			String message = result.getAllErrors().stream().map(error -> error.getDefaultMessage()).collect(Collectors.joining("\n"));
-			return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+			List<String> message = result.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList());
+			return new RequestedMessage(false,message).toMessage();
 		}
 	}
 }
